c91080fd835cf80ed718189862750cd398142271
BanuTej007 <banutej2003@gmail.com>
2023-10-13 22:59:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -11,10 +11,3 @@ function App() {
 
 export default App;
 
-echo "# react1" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:BanuTej007/react1.git
-git push -u origin main
